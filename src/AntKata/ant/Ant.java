de9f2ec1a22a3256fdedd9292ee6520dbac2d76f
Peter Balivet <peterbalivet@gmail.com>
2020-05-24 15:08:55 +0200

    All tests work, ants not comming back to colony

--- src/AntKata/ant/Ant.java
+++ src/AntKata/ant/Ant.java
@@ -10,18 +10,70 @@ public class Ant {
     private Point position;
     private Status status;
     private Point lastKnownFoodPosition;
+    private Point positionColony;
 
     // TODO Attributs
 
     public Ant(Point positionColony) {
         // TODO
+        this.position = this.positionColony = positionColony;
+        this.status = Status.WANDERING;
     }
 
-    private void scatter() {
+    public void scatter() {
         int randomX = RNG.random(-1, 1);
         int randomY = RNG.random(-1, 1);
 
+        switch(this.getStatus()) {
+            case WANDERING:
+                this.position = new Point(this.getPositionX()+randomX, this.getPositionY()+randomY);
+                break;
+            case RETURNING_COLONY:
+                this.position = new Point(this.getPositionX()-randomX, this.getPositionY()-randomY);
+                break;
+            case FETCHING_FOOD:
+                this.position = new Point(this.getPositionX()+randomX, this.getPositionY()+randomY);
+                break;
+            default:
+        }
+
         // TODO
+    }
+
+    //If food found status changes and last known food position is set
+    private void foundFood(Point foodLocation){
+        this.lastKnownFoodPosition = foodLocation;
+        this.status = Status.RETURNING_COLONY;
+        //System.out.println("Found Food");
+    }
+
+    //Checks if ant on food
+    public void findFood(List<Point> food){
+        for (Point foodLocation : food) {
+            if (foodLocation.equals(this.position)) this.foundFood(foodLocation);
+            //System.out.println("test");
+        }
+    }
+
+    //checks if ant on colony cell
+    public boolean isOnColony(){
+
+        if (this.position.equals(this.positionColony)){
+            this.status = Status.FETCHING_FOOD;
+            return true;
+        }
+        return false;
+    }
+
+    //Checks if ant on food cell
+    public void isOnFood(List<Point> food){
+        if (this.position.equals(this.lastKnownFoodPosition)){
+            if (food.contains(this.lastKnownFoodPosition)){
+                this.foundFood(this.lastKnownFoodPosition);
+            }else{
+                this.status = Status.WANDERING;
+            }
+        }
     }
 
     // TODO Méthodes de classes
@@ -38,12 +90,20 @@ public class Ant {
         return this.position;
     }
 
+    public void setStatus(Status status){
+        this.status = status;
+    }
+
     public Status getStatus() {
         return this.status;
     }
 
     public void setPosition(Point point) {
         this.position = new Point(point.x, point.y);
+    }
+
+    public void setLastKnownFoodPosition(Point lastKnownFoodPosition){
+        this.lastKnownFoodPosition = lastKnownFoodPosition;
     }
 
     public Point getLastKnownFoodPosition() {
--- src/AntKata/ant/Colony.java
+++ src/AntKata/ant/Colony.java
@@ -18,12 +18,48 @@ public class Colony {
         // TODO
         this.ants = new ArrayList<>();
         this.position = position;
+        this.foodCollected = 0;
+        for(int i = 0; i< nbAnts; i++){
+            ants.add(new Ant(position));
+        }
     }
 
+    //Every next turn
     public int next(List<Point> food) {
         // TODO
+        for (Ant a : ants){
+            switch(a.getStatus()) {
+                case WANDERING:
+                    a.findFood(food);
+                    break;
+                case RETURNING_COLONY:
+                    if(a.isOnColony()) this.foodCollected ++;
+                    break;
+                case FETCHING_FOOD:
+                    a.isOnFood(food);
+                    break;
+                default:
+        }
+        //If two ants are in the same cell, communicate food location
+        for (int i = 0; i<ants.size()-1; i ++){
+            for (int j = i + 1; j<ants.size(); j ++){
+                if(ants.get(i).getPosition().equals(ants.get(j).getPosition())){
+                    if(ants.get(i).getStatus() == Status.FETCHING_FOOD || ants.get(i).getStatus() == Status.RETURNING_COLONY){
+                        ants.get(j).setLastKnownFoodPosition(ants.get(i).getLastKnownFoodPosition());
+                        ants.get(j).setStatus(Status.FETCHING_FOOD);
+                    }else{
+                        if(ants.get(j).getStatus() == Status.FETCHING_FOOD || ants.get(j).getStatus() == Status.RETURNING_COLONY){
+                            ants.get(i).setLastKnownFoodPosition(ants.get(j).getLastKnownFoodPosition());
+                            ants.get(i).setStatus(Status.FETCHING_FOOD);
+                        }
+                    }
+                }
+            }
+        }
+            a.scatter();
+        }
 
-        return foodCollected;
+        return this.foodCollected;
     }
 
     public List<Ant> getAnts() {
